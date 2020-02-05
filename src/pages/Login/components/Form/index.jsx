8dc261f65220ab6fa875427d994e429d11ad2626
kimberlyrojasra <kimberlyrojasra@gmail.com>
2020-02-05 16:02:51 -0500

    grillas formulario, inputs

--- src/pages/Login/components/Form/index.jsx
+++ src/pages/Login/components/Form/index.jsx
@@ -10,25 +10,49 @@ const Form = () => (
     </p>
     <p className='container__subtitle'>Ingresa los datos para comenzar</p>
     <section className='container__section'>
-      <select>
-        {
-          [1, 2, 3].map((doc) => <option key={doc}>{doc}</option>)
-        }
-      </select>
-      <input type='text' placeholder='Nro. de Documento' />
-      <input type='date' placeholder='Fecha de nacimiento' />
-      <input type='text' placeholder='Celular' />
-      <input type='checkbox' />
-      <p>
-      Acepto la
-        <u>Política de Protección de Datos Personales y los Términos y Condiciones.</u>
-      </p>
-      <input type='checkbox' />
-      <p>
-      Acepto la
-        <u>Política de Envío de Comunicaciones Comerciales.</u>
-      </p>
-			<button>Comencemos</button>
+      <div className='container__section--document'>
+        <div className='container__section--typeDoc'>
+          <select>
+            {
+              [1, 2, 3].map((doc) => <option key={doc}>{doc}</option>)
+            }
+          </select>
+        </div>
+        <div className='container__section--numberId'>
+          <input type='text' placeholder='Nro. de Documento' />
+        </div>
+      </div>
+      <div className='container__section--dateborn'>
+        <input type='date' placeholder='Fecha de nacimiento' />
+      </div>
+      <div className='container__section--telephone'>
+        <input type='text' placeholder='Celular' />
+      </div>
+      <div className='container__section--politic1 politics'>
+        <div className='container__section--checkbox'>
+          <input type='checkbox' />
+        </div>
+        <div className='container__section--description'>
+          <p>
+          Acepto la
+            <u>Política de Protección de Datos Personales y los Términos y Condiciones.</u>
+          </p>
+        </div>
+      </div>
+      <div className='container__section--politic2 politics'>
+        <div className='container__section--checkbox'>
+          <input type='checkbox' />
+        </div>
+        <div className='container__section--description'>
+          <p>
+          Acepto la
+            <u>Política de Envío de Comunicaciones Comerciales.</u>
+          </p>
+        </div>
+      </div>
+      <div className='container__section--button'>
+        <button type='button'>Comencemos</button>
+      </div>
     </section>
   </form>
 );
