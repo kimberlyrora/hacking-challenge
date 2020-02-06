5b4ab6e0af8c7a2ffc8d9e4fd2b72a01ebc3b12e
kimberlyrojasra <kimberlyrojasra@gmail.com>
2020-02-06 18:17:39 -0500

    optimizando bullets en cover

--- src/pages/Login/components/Cover/index.jsx
+++ src/pages/Login/components/Cover/index.jsx
@@ -1,5 +1,8 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable indent */
+/* eslint-disable camelcase */
 /* eslint-disable jsx-quotes */
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 import frame from '../../../../assets/frame.svg';
 import dad from '../../../../assets/dad.svg';
@@ -8,9 +11,24 @@ import kid from '../../../../assets/kid.svg';
 import shadow from '../../../../assets/shadow.svg';
 import shadow2 from '../../../../assets/shadow2.svg';
 import flecha from '../../../../assets/flecha.png';
+import disabled_arrow from '../../../../assets/disabled_arrow.svg';
 
 const Cover = (props) => {
   const { bullets } = props;
+  const [bullet, setBullet] = useState(1);
+
+  const benefit = bullets.filter((ben) => ben.id === bullet)
+                         .map((bene) => (
+                           <div key={bene.id} className='container__cover--li'>
+                             <img src={bene.icon} alt='icon' />
+                             <li>{bene.description}</li>
+                           </div>
+                         ));
+
+  const onClick = (e) => {
+    if (e === 'R') return setBullet(bullet + 1);
+    return setBullet(bullet - 1);
+  };
   return (
     <>
       <div className='container__cover--description'>
@@ -19,19 +37,40 @@ const Cover = (props) => {
           <b> Salud</b>
         </p>
         <ul className='container__cover--ul'>
-          {
-            bullets.map((bullet) => (
-              <div key={bullet.id}>
-                <img src={bullet.icon} alt='icon' />
-                <li className='container__cover--li'>{bullet.description}</li>
-              </div>
-            ))
-          }
+          {benefit}
         </ul>
         <nav className='container__cover--nav'>
-          <img className='container__cover--arrow1' src={flecha} alt='arrow' />
-            01 / 04
-          <img className='container__cover--arrow2' src={flecha} alt='arrow' />
+          <button
+            type='button'
+            className='container__cover--button1 global_button'
+            onClick={onClick}
+            disabled={bullet <= 1}
+          >
+            <img
+              className={
+                bullet <= 1 ? 'global_arrow'
+                            : 'container__cover--arrow1 global_arrow'
+              }
+              src={bullet <= 1 ? disabled_arrow : flecha}
+              alt='arrow'
+            />
+          </button>
+          <span className='container__cover--span'>{`0${bullet} / 0${bullets.length}`}</span>
+          <button
+            type='button'
+            className='container__cover--button2 global_button'
+            onClick={() => onClick('R')}
+            disabled={bullet >= 4}
+          >
+            <img
+              className={
+                bullet >= 4 ? 'container__cover--disablearrow2 global_arrow'
+                            : 'container__cover--arrow2 global_arrow'
+              }
+              src={bullet >= 4 ? disabled_arrow : flecha}
+              alt='arrow'
+            />
+          </button>
         </nav>
       </div>
       <div className='container__cover--img'>
