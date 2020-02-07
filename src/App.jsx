b72dd4d019c98d767c7566baa02c33c8133b4122
kimberlyrojasra <kimberlyrojasra@gmail.com>
2020-02-07 07:49:45 -0500

    react add Family

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
 /* eslint-disable jsx-quotes */
-import React from 'react';
-import { Router, Redirect } from '@reach/router';
+import React, { Suspense } from 'react';
+import { Router } from '@reach/router';
 import './App.css';
-import Login from './pages/Login';
+// import Login from './pages/Login';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import AddFamily from './pages/AddFamily';
 
 const data = require('./db.json');
 
+const Login = React.lazy(() => import('./pages/Login'))
+
 const App = () => (
-  <div className='app'>
+  <Suspense fallback={<div>Cargando...</div>}>
+    <div className='app'>
     <div className='app__header'>
       <Header />
     </div>
     <Router>
       {/* <div className='app__body'> */}
         <Login path='/' data={data[0]} />
-        <AddFamily path='/addFamily' />
+        <AddFamily path='/addFamily' data={data[0]} />
       {/* </div> */}
     </Router>
     <div className='app__footer'>
       <Footer />
     </div>
   </div>
+  </Suspense>
 );
 
 export default App;
--- src/pages/AddFamily/index.jsx
+++ src/pages/AddFamily/index.jsx
@@ -1,11 +1,34 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable jsx-quotes */
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 import back from '../../assets/back.svg';
 import { Link } from '@reach/router';
+import Input from '../../components/Input';
 
 const AddFamily = (props) => {
+  const { data: { inputs } } = props;
+  const [state, setState] = useState({
+    born: '',
+    name: '',
+    lastname1: '',
+    lastname2: '',
+    document: '',
+    radio1: '',
+    radio2: '',
+    radio3: '',
+    radio4: '',
+  });
+  
+  const setValue = (e) => {
+    const { target: { type, checked, value, name } } = e;
+    const newState = type === 'checkbox' ? checked : value;
+    setState({
+      ...state,
+      [name]: newState,
+    });
+  };
+
   return (
     <section className='container__addFamily'>
       <section className='container__subheader'>
@@ -21,7 +44,24 @@ const AddFamily = (props) => {
         </div>
       </section>
       <section className='container__body'>
-					dddd
+        <p className='container__body--title'>Datos personales del titular</p>
+        <div className='container__body--form'>
+          {
+            inputs.filter((inp) => inp.step1)
+                  .map((input) => (
+                    <Input
+                      name={input.name}
+                      key={input.id}
+                      attributes={input}
+                      value={state[input.name]}
+                      setValues={setValue}
+                  />
+                  ))
+          }
+        </div>
+        <div className='container__body--gender'>item</div>
+        <div className='container__body--ensure'>item</div>
+        <div className='container__body--button'>item</div>
       </section>
     </section>
   );
